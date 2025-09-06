5e964c2c195404c9ef8ad6f33deec55965f6041a
Allison <allysonolliveira3@gmail.com>
2025-09-06 19:16:34 -0300

    projeto inicial

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um numero de 1 até 10');
+
+if (chute == numeroSecreto) {
+    console.log('Isso aí!')
+} else {
+    alert('Errou!')
+}
